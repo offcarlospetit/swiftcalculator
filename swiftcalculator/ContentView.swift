c2f190c48dbb8dd97ce5368f0b55e1af3f5cefa1
Carlos Petit <ca.alberto.p@gmail.com>
2020-05-02 12:59:06 -0400

    Initial Commit

---
+++ swiftcalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftcalculator
+//
+//  Created by Carlos Petit on 02-05-20.
+//  Copyright © 2020 Carlos Petit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
